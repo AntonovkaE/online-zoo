655f72f297feec9abe1b6e2f12769a87406b7952
Elvira <sabirova.elvira.gam@gmail.com>
2022-10-31 15:28:54 +0500

    refactor: add some styles

---
+++ pages/main/script.js
@@ -0,0 +1,9 @@
+// let multi = (first = 1, second = 2) => first * second;
+// console.log(multi(3,5))
+// console.log(func);
+// const func = function () {
+//   console.log(parseFloat('180.727.312'))
+// }
+// func()
+console.log(12.123456.toFixed(3))
+
